343622e9acec3d5d587e4b52e9d0eb7445424ea7
André Luiz <andreluiz.ddev@gmail.com>
2024-11-17 05:00:25 -0300

    up video and image

--- src/components/PlanCard.vue
+++ src/components/PlanCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    :class="[isPrimary ? 'bg-gradient-to-br from-emerald-700 to-emerald-600 border border-emerald-500' : 'bg-gradient-to-br from-emerald-800 to-emerald-700 border-emerald-600']"
+    :class="[isPrimary ? 'bg-gradient-to-br from-emerald-700 to-emerald-600 border border-emerald-500' : 'bg-gradient-to-br from-emerald-800 to-emerald-700 border border-emerald-600']"
     class="rounded-lg p-4">
     <div class="mb-6">
       <h3 :class="[isPrimary ? 'text-white' : 'text-emerald-100']" class="text-2xl">{{ title }}</h3>
@@ -11,11 +11,14 @@
         <CheckIcon class="h-6 w-6 text-green-500" /> {{ feature }}
       </li>
     </ul>
-    <p :class="[isPrimary ? 'text-white' : 'text-emerald-100']" class="text-2xl font-bold mb-4">{{ price }}</p>
-    <button
-      :class="[isPrimary ? 'bg-white hover:bg-emerald-100 text-emerald-800' : 'bg-emerald-600 text-white hover:bg-emerald-500']"
-      class="w-full py-2 rounded">
-      Escolher Plano</button>
+    <span v-if="isLoading" :class="[isLoading ? 'loader text-2xl font-bold mb-4' : '']"></span>
+    <p v-else :class="[isPrimary ? 'text-white' : 'text-emerald-100']" class="text-2xl font-bold mb-4">{{ price }}</p>
+    <RouterLink :to="`/payment/${plan}`">
+      <button
+        :class="[isPrimary ? 'bg-white hover:bg-emerald-100 text-emerald-800' : 'bg-emerald-600 text-white hover:bg-emerald-500']"
+        class="w-full py-2 rounded">
+        Escolher Plano</button>
+    </RouterLink>
   </div>
 </template>
 
@@ -33,6 +36,11 @@ export default defineComponent({
     isPrimary: {
       type: Boolean,
       default: false
+    },
+    plan: String,
+    isLoading: {
+      type: Boolean,
+      default: false
     }
   },
   components: {
@@ -41,6 +49,55 @@ export default defineComponent({
 });
 </script>
 
+
 <style scoped>
-/* Adicione estilos customizados aqui, se necessário */
+.loader {
+  width: 32px;
+  height: 32px;
+  border: 5px solid #FFF;
+  border-radius: 50%;
+  display: inline-block;
+  box-sizing: border-box;
+  position: relative;
+  animation: pulse 1s linear infinite;
+}
+
+.loader:after {
+  content: '';
+  position: absolute;
+  width: 32px;
+  height: 32px;
+  border: 5px solid #FFF;
+  border-radius: 50%;
+  display: inline-block;
+  box-sizing: border-box;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  animation: scaleUp 1s linear infinite;
+}
+
+@keyframes scaleUp {
+  0% {
+    transform: translate(-50%, -50%) scale(0)
+  }
+
+  60%,
+  100% {
+    transform: translate(-50%, -50%) scale(1)
+  }
+}
+
+@keyframes pulse {
+
+  0%,
+  60%,
+  100% {
+    transform: scale(1)
+  }
+
+  80% {
+    transform: scale(1.2)
+  }
+}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="min-h-screen">
-        <div class="container mx-auto px-36 sm:px-10 2xl:px-44 sml:px-4 py-16">
+        <div class="py-16">
             <header class="text-center mb-20">
                 <div class="flex justify-center mb-6">
                     <img src="../assets/LogoWebHulkTest.PNG" alt="WebHulk Test Logo" width="200" height="200"
@@ -12,55 +12,120 @@
                     forma
                     assíncrona através de webhooks.
                 </p>
-                <div class="grid md:grid-cols-3 gap-8">
-                    <InfoCard icon="Code" title="Para Desenvolvedores"
-                        description="Projetado para cenários em que você ainda não possui um serviço próprio em produção e está realizando testes em ambiente local." />
-                    <InfoCard icon="Webhook" title="Teste de Webhooks"
-                        description="Simule e valide respostas de webhook de plataformas como Stripe, PayPal e outras APIs assíncronas." />
-                    <InfoCard icon="Shield" title="Ambiente Seguro"
-                        description="Oferece um ambiente seguro e eficiente para testar, acelerando seu processo de desenvolvimento e testes." />
-                </div>
+
+                <section class="container mx-auto px-36 sm:px-10 2xl:px-44 sml:px-4">
+                    <div class="grid md:grid-cols-3 gap-8">
+                        <InfoCard icon="Code" title="Para Desenvolvedores"
+                            description="Projetado para cenários em que você ainda não possui um serviço próprio em produção e está realizando testes em ambiente local." />
+                        <InfoCard icon="Webhook" title="Teste de Webhooks"
+                            description="Simule e valide respostas de webhook de plataformas como Stripe, PayPal e outras APIs assíncronas." />
+                        <InfoCard icon="Shield" title="Ambiente Seguro"
+                            description="Oferece um ambiente seguro e eficiente para testar, acelerando seu processo de desenvolvimento e testes." />
+                    </div>
+                </section>
             </header>
-            <section class="mb-16">
-                <h2 class="text-2xl font-semibold text-white mb-6 text-center">Recursos Principais</h2>
-                <div class="grid md:grid-cols-3 gap-8">
-                    <FeatureCard icon="Globe" title="API Flexível"
-                        description="Faça requisições para verificar o status do seu webhook" />
-                    <FeatureCard icon="Zap" title="Conexão Local"
-                        description="Conecte-se ao seu webhook local enquanto a aba estiver aberta" />
-                    <FeatureCard icon="Lock" title="Privacidade Garantida"
-                        description="Todos os posts são apagados após 3 minutos de leitura" />
+
+
+            <section
+                class="bg-gradient-to-br from-emerald-900 to-emerald-1000 border-b border-t border-emerald-600 py-16">
+                <div class="mb-16 container mx-auto px-36 sm:px-10 2xl:px-44 sml:px-4">
+                    <h2 class="text-2xl font-semibold text-white mb-6 text-center">Recursos Principais</h2>
+                    <div class="grid md:grid-cols-3 gap-8">
+                        <FeatureCard icon="Globe" title="API Flexível"
+                            description="Faça requisições para verificar o status do seu webhook" />
+                        <FeatureCard icon="Zap" title="Conexão Local"
+                            description="Conecte-se ao seu webhook local enquanto a aba estiver aberta" />
+                        <FeatureCard icon="Lock" title="Privacidade Garantida"
+                            description="Todos os posts são apagados após 3 minutos de leitura" />
+                    </div>
                 </div>
             </section>
-            <section class="mb-16">
-                <h2 class="text-2xl font-semibold text-white mb-6 text-center">Nossos Planos</h2>
-                <div class="grid md:grid-cols-2 gap-8">
-                    <RouterLink to="/payment/free">
+
+
+
+            <section class="mb-16 container mx-auto px-36 sm:px-10 2xl:px-44 sml:px-4 pt-16">
+                <h2 class="text-2xl font-semibold text-white mb-6 text-center">Como funciona!</h2>
+                <div class="flex items-center grid md:grid-cols-2">
+                    <img src="@/assets/WebHulkTest.svg"
+                        class="rounded rounded-lg border border-emerald-600 cursor-zoom-in"
+                        @click="openModal('image', '@/assets/WebHulkTest.svg')"
+                        style="width: 85%; height: auto; object-fit: cover; ">
+
+                    <video autoplay loop muted playsinline
+                        class="rounded rounded-lg border border-emerald-600 ml-4 cursor-zoom-in"
+                        @click="openModal('video', '@/assets/WebHulkTestDashboard.mp4')"
+                        style="width: 97%; height: auto; object-fit: cover;">
+                        <source src="@/assets/WebHulkTestDashboard.mp4" type="video/mp4">
+                    </video>
+                </div>
+
+                <p class="text-lg text-emerald-100 mt-4">
+                    Você pode receber suas mensagens de duas maneiras:
+                </p>
+                <ol class="list-decimal list-inside text-lg text-emerald-100 mt-2">
+                    <li>
+                        <strong>Webhook Local:</strong> Implemente um webhook local (com exemplos prontos
+                        disponíveis) e configure no WebHulkTest. Enquanto a aba do navegador estiver aberta, todas
+                        as mensagens recebidas serão direcionadas para seu webhook local, proporcionando um teste
+                        semelhante ao ambiente de produção.
+                    </li>
+                    <li>
+                        <strong>Requisição GET:</strong> Realize uma requisição GET simples à WebHulkTestApi para
+                        receber todas as novas mensagens. Este método é mais limitado, pois os dados serão agrupados
+                        em uma lista e enviados em uma única resposta, porém é mais simples e te dá um
+                        possibilidades de fazer qualquer coisa com WebHulkTest, muito além da proposta inicial.
+                    </li>
+                </ol>
+            </section>
+
+
+            <section
+                class="bg-gradient-to-br from-emerald-900 to-emerald-1000 border-b border-t border-emerald-600 pt-16">
+                <div class="mb-16 container mx-auto px-36 sm:px-10 2xl:px-44 sml:px-4">
+                    <h2 class="text-2xl font-semibold text-white mb-6 text-center">Nossos Planos</h2>
+                    <div class="grid md:grid-cols-2 gap-8">
                         <PlanCard title="Plano Grátis" description="Perfeito para começar"
                             :features="['50 requisições na API por dia', 'Até 5 posts no webhook por dia', 'Privacidade e segurança padrão']"
-                            price="Grátis" />
-                    </RouterLink>
+                            price="Grátis" plan="free" />
 
-                    <RouterLink to="/payment/full">
                         <PlanCard title="Plano Full" description="Para usuários avançados"
                             :features="['50 requisições na API por minuto', '150 posts no webhook por minuto', 'Privacidade e segurança avançadas']"
-                            :price="price" isPrimary />
-                    </RouterLink>
+                            :price="price" plan="full" :isLoading="isLoading" isPrimary />
+                    </div>
                 </div>
             </section>
+
+
             <footer class="text-center text-emerald-100">
                 <p>&copy; 2024 WebHulkTest Test Service. Todos os direitos reservados.</p>
             </footer>
         </div>
     </div>
+
+
+    <div v-if="isModalOpen" class="modal" @click="closeModal">
+        <span class="close" @click="closeModal">&times;</span>
+        <template v-if="modalType === 'image'">
+            <img class="rounded rounded-lg border border-emerald-600 ml-4 modal-content"
+                src="@/assets/WebHulkTest.svg" />
+        </template>
+        <template v-else-if="modalType === 'video'">
+            <video class="rounded rounded-lg border border-emerald-600 ml-4 modal-content" autoplay loop muted
+                playsinline>
+                <source src="@/assets/WebHulkTestDashboard.mp4" type="video/mp4">
+                Seu navegador não suporta a tag de vídeo.
+            </video>
+        </template>
+    </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 import InfoCard from '@/components/InfoCard.vue';
 import FeatureCard from '@/components/FeatureCard.vue';
 import PlanCard from '@/components/PlanCard.vue';
 import { RouterLink } from 'vue-router'
+// import WebHulkTestApresentation from '@/assets/WebHulkTestApresentation.vue';
 
 export default defineComponent({
     name: 'LandingPage',
@@ -69,9 +134,27 @@ export default defineComponent({
         FeatureCard,
         PlanCard
     },
+    setup() {
+        const isModalOpen = ref(false);
+        const modalSrc = ref('');
+        const modalType = ref('');
+
+        const openModal = (type: string, src: string) => {
+            modalType.value = type;
+            modalSrc.value = src;
+            isModalOpen.value = true;
+        };
+
+        const closeModal = () => {
+            isModalOpen.value = false;
+        };
+
+        return { isModalOpen, modalSrc, modalType, openModal, closeModal };
+    },
     data() {
         return {
-            price: '$ ? '
+            price: '$ 5 ',
+            isLoading: ref(false)
         };
     },
     mounted() {
@@ -79,6 +162,8 @@ export default defineComponent({
     },
     methods: {
         getUserCountry(): void {
+            this.isLoading = true;
+
             if (navigator.geolocation) {
                 navigator.geolocation.getCurrentPosition(this.showPosition, this.handleError);
             } else {
@@ -88,6 +173,7 @@ export default defineComponent({
 
         handleError(error: GeolocationPositionError): void {
             console.error(`ERROR(${error.code}): ${error.message}`);
+            this.isLoading = false;
         },
 
         showPosition(position: GeolocationPosition): void {
@@ -95,8 +181,9 @@ export default defineComponent({
             const longitude: number = position.coords.longitude;
 
             const country = localStorage.getItem('country');
-            if (country){
+            if (country) {
                 this.setPrice(country)
+                this.isLoading = false;
                 return;
             }
 
@@ -106,18 +193,18 @@ export default defineComponent({
                     const country: string = data?.address.country;
                     localStorage.setItem('country', country);
                     this.setPrice(country);
+
+                    this.isLoading = false;
                 })
                 .catch((error) => {
-                    debugger;
                     console.error('Erro ao obter o país:', error);
+                    this.isLoading = true;
                 });
         },
 
         setPrice(country: string): void {
             if (country === "Brasil") {
                 this.price = "R$ 5 BRL";
-            } else if (country === "Argentina") {
-                this.price = "$ 1,50 USD";
             } else {
                 this.price = "$ 5 USD";
             }
@@ -126,4 +213,31 @@ export default defineComponent({
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+.modal {
+    display: flex;
+    position: fixed;
+    z-index: 1000;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.8);
+    justify-content: center;
+    align-items: center;
+}
+
+.modal-content {
+    max-width: 90%;
+    max-height: 90%;
+}
+
+.close {
+    position: absolute;
+    top: 20px;
+    right: 30px;
+    color: white;
+    font-size: 40px;
+    cursor: pointer;
+}
+</style>
