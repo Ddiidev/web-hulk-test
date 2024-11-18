5cfbd45c0632771832561b344eb437ce92cb3d11
André Luiz <andreluiz.ddev@gmail.com>
2024-11-18 10:47:29 -0300

    fix build 2x

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { createApp } from 'vue'
 import { createI18n } from 'vue-i18n'
 import App from './App.vue'
 import router from './router'
-import { plugin as VueTippy } from 'vue-tippy';
 
 
 import {pt_BR} from './locale/pt_BR';
@@ -29,7 +28,5 @@ const app = createApp(App)
 app.use(router)
 
 app.use(i18n);
-
-app.use(VueTippy);
 
 app.mount('#app')
