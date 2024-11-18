72f4639e2cc09f7905b9d69031a17fd6d2118dc1
André Luiz <andreluiz.ddev@gmail.com>
2024-11-18 10:50:36 -0300

    fix build x4

--- src/components/LanguageSelect.vue
+++ src/components/LanguageSelect.vue
@@ -14,7 +14,7 @@
                     Português (BR)
                 </div>
                 <div @click="selectLanguage('en_US')" class="flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-green-100 cursor-pointer">
-                    <img src="../assets/icons/us.png" alt="English" class="w-5 h-5 mr-2" />
+                    <img src="../assets/Icons/us.png" alt="English" class="w-5 h-5 mr-2" />
                     English
                 </div>
             </div>
